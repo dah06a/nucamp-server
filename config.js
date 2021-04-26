5a7ba20b077dc5225945e1ec0a1265a4fb42d6f5
David Henry <dah06a@acu.edu>
2021-04-26 18:48:25 -0500

    OAuth With Passport And Facebook Exercise Complete.

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '295922398734768',
+        clientSecret: '6daff570341083cf2003c0bdf28e35b6'
+    }
 }
